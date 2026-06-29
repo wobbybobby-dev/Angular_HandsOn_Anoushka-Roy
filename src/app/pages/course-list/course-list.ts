39a0be4e25acb0b5f694144d8604113d94b6b35f
wobbybobby-dev <23052866@kiit.ac.in>
2026-06-29 12:40:57 +0530

    Updated Hands-On week2--Lifecycle Hooks

--- src/app/pages/course-list/course-list.ts
+++ src/app/pages/course-list/course-list.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CourseCard } from "../../components/course-card/course-card";
 
 @Component({
   selector: 'app-course-list',
-  imports: [],
+  imports: [CourseCard],
   templateUrl: './course-list.html',
   styleUrl: './course-list.css',
 })
