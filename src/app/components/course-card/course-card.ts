42469b08fb40aa09a2e7f78b8e0d19730b41da2f
wobbybobby-dev <23052866@kiit.ac.in>
2026-07-02 01:02:12 +0530

    Updated week2 Hands-On

--- src/app/components/course-card/course-card.ts
+++ src/app/components/course-card/course-card.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-course-card',
@@ -7,7 +7,8 @@ import { Component, OnChanges, Input, SimpleChanges } from '@angular/core';
   styleUrl: './course-card.css',
 })
 export class CourseCard implements OnChanges{
-  @Input() course : any;   //tells this variable can receive data from a parent component
+
+  //@Input() course : any;   //tells this variable can receive data from a parent component
 
   /*changes.course is an object, with properties like previousValue, currentValue, firstChange, etc*/
 
@@ -15,5 +16,16 @@ export class CourseCard implements OnChanges{
     console.log("Previous Course: ", changes['course'].previousValue);
     console.log("Current Course: ", changes['course'].currentValue);
   }
+
+  @Input() course!:
+  {
+    id: number;
+    name : string;
+    code : string;
+    credits: number;
+
+  };
+
+  @Output() enrollRequested = new EventEmitter<number>() 
 
 }
--- src/app/pages/course-list/course-list.ts
+++ src/app/pages/course-list/course-list.ts
@@ -3,8 +3,48 @@ import { CourseCard } from "../../components/course-card/course-card";
 
 @Component({
   selector: 'app-course-list',
-  imports: [CourseCard],
+  imports: [CourseCard ],
   templateUrl: './course-list.html',
   styleUrl: './course-list.css',
 })
-export class CourseList {}
+export class CourseList {
+  selectedCourseId: number | null = null;
+  courses=[
+    {
+      id: 10,
+      name : "Java",
+      code : "CS1042",
+      credits: 3
+    },
+    {
+      id: 11,
+      name : "Python",
+      code : "CS1044",
+      credits: 3
+    },
+    {
+      id: 12,
+      name : "Computer Networks",
+      code : "CS2378",
+      credits: 2
+    },
+    {
+      id: 13,
+      name : "Machine Learning",
+      code : "CS1665",
+      credits: 4
+    },
+    {
+      id: 14,
+      name : "Software Engg.",
+      code : "CS4566",
+      credits: 2
+    }
+  ]
+
+  onEnroll(courseId: number)
+  {
+    console.log("Enrolling in course: "+ courseId);
+    this.selectedCourseId= courseId;
+  }
+}
